dbe37325fe039b0a22e1f7530fc8b621573ee17a
Vlad <seven712234234@gmail.com>
2023-07-05 15:31:47 +0300

    added setup.py, config.ini, requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='VeroWebTesting',
+    version='0.0.1',
+    install_requires=[
+        'pytest',
+        'selenium',
+        'webdriver-manager',
+        'pyperclip'
+    ],
+)
